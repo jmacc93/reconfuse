6a17944be17088fa7b5e93c2ac52a042cc5bd69e
jmacc93 <joe_miller_93@protonmail.com>
2022-12-06 08:46:58 -0600

    Indentation fix

--- pagelets/selfpad.mjs
+++ pagelets/selfpad.mjs
@@ -21,43 +21,43 @@ async function renderTextareaContent(textarea, contentDisplay, pagelet) {
 }
 
 export async function installFunctionality(callElem) {
-    const lib = await import('/lib/lib.mjs')
-    
-    const pagelet        = callElem.parentElement
-    const contentDisplay = pagelet.querySelector('.content-display')
-    const textarea       = pagelet.querySelector('textarea.input')
-    
-    const padName = pagelet.dataset.name
-    
-    // const textExpand = lib.makeTextExpander({
-    //   ...lib.stdTextExpansions,
-    //   ["!PAD"]: {to: ()=>`${pagelet.dataset.name}`, endPosition: -1}
-    // })
-          
-    let fromStorage = localStorage.getItem(padName)
-    if(fromStorage !== null) {
-      textarea.value = fromStorage
-      if(textarea.value !== '')
-        renderTextareaContent(textarea, contentDisplay, pagelet)
-    }
-    
-    // textarea.addEventListener('input', inputEvent => {
-    //   let oldCaretPos = textarea.selectionStart
-    //   let repOffset   = 0;
-    //   [textarea.value, repOffset] = textExpand.withOffset(textarea.value)
-    //   textarea.selectionStart = oldCaretPos + repOffset
-    //   textarea.selectionEnd   = oldCaretPos + repOffset
-    // })
-    textarea.addEventListener('keydown', keyEvent => {
-      if(keyEvent.ctrlKey && keyEvent.key === 'Enter')
-        renderTextareaContent(textarea, contentDisplay, pagelet)
-    })
+  const lib = await import('/lib/lib.mjs')
+  
+  const pagelet        = callElem.parentElement
+  const contentDisplay = pagelet.querySelector('.content-display')
+  const textarea       = pagelet.querySelector('textarea.input')
+  
+  const padName = pagelet.dataset.name
+  
+  // const textExpand = lib.makeTextExpander({
+  //   ...lib.stdTextExpansions,
+  //   ["!PAD"]: {to: ()=>`${pagelet.dataset.name}`, endPosition: -1}
+  // })
+        
+  let fromStorage = localStorage.getItem(padName)
+  if(fromStorage !== null) {
+    textarea.value = fromStorage
+    if(textarea.value !== '')
+      renderTextareaContent(textarea, contentDisplay, pagelet)
   }
   
-  export async function updateButton(callElem) {
-    const pagelet = callElem.closest('.selfpad')
-    const textarea = pagelet.querySelector('textarea')
-    const contentDisplay = pagelet.querySelector('.content-display')
-    
-    renderTextareaContent(textarea, contentDisplay, pagelet)
-  }
+  // textarea.addEventListener('input', inputEvent => {
+  //   let oldCaretPos = textarea.selectionStart
+  //   let repOffset   = 0;
+  //   [textarea.value, repOffset] = textExpand.withOffset(textarea.value)
+  //   textarea.selectionStart = oldCaretPos + repOffset
+  //   textarea.selectionEnd   = oldCaretPos + repOffset
+  // })
+  textarea.addEventListener('keydown', keyEvent => {
+    if(keyEvent.ctrlKey && keyEvent.key === 'Enter')
+      renderTextareaContent(textarea, contentDisplay, pagelet)
+  })
+}
+  
+export async function updateButton(callElem) {
+  const pagelet = callElem.closest('.selfpad')
+  const textarea = pagelet.querySelector('textarea')
+  const contentDisplay = pagelet.querySelector('.content-display')
+  
+  renderTextareaContent(textarea, contentDisplay, pagelet)
+}
