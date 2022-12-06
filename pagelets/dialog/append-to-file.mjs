ae9dafec8d061ea2bc2760853967915f37c3c91f
jmacc93 <joe_miller_93@protonmail.com>
2022-12-06 10:55:49 -0600

    Ctrl-enter triggers a tagged append

--- pagelets/dialog/append-to-file.mjs
+++ pagelets/dialog/append-to-file.mjs
@@ -1,15 +1,15 @@
 
-export async function taggedAppend(callElemButton) {
+export async function taggedAppend(callElem) {
   const lib = await import('/lib/lib.mjs')
-  const pagelet = callElemButton.closest('.pagelet[data-file]')
+  const pagelet = callElem.closest('.pagelet[data-file]')
   const textarea = pagelet.querySelector(':scope > textarea')
   let response = await fetch(`/bin/file.s.js/append?file=${pagelet.dataset.file}&tagged`, {method:"POST", body: textarea.value})
   if(response.ok) {
-    lib.notificationFrom(callElemButton, 'Appended', {transient: true})
+    lib.notificationFrom(callElem, 'Appended', {transient: true})
     textarea.value = ''
     return void 0
   } else {
-    return void lib.notificationFrom(callElemButton, `Error: ${response.status}, ${response.statusText}`, {error: true})
+    return void lib.notificationFrom(callElem, `Error: ${response.status}, ${response.statusText}`, {error: true})
   }
 }
 
