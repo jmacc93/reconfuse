20ed656697a2add6de95546dd78c3dd22d957124
jmacc93 <joe_miller_93@protonmail.com>
2022-12-19 12:05:29 -0600

    Remove old code

--- pagelets/represent-directory.mjs
+++ pagelets/represent-directory.mjs
@@ -35,46 +35,6 @@ export async function addOpenButtonFunctionality(callElem) {
   })
 }
 
-
-export async function openSimpleEditDialog(dropdownCallElem) {
-  const lib = await import('/lib/lib.mjs')
-  const dialog = await import('/pagelets/dialog/dialog.mjs')
-  const file   = lib.getParentMatching(dropdownCallElem, '*[data-file]')?.dataset.file
-  if(file === undefined)
-    return void lib.notificationFrom(dropdownCallElem, 'Error, no data-file attribute given', {error: true})
-  // else
-  const initialValue = await fetch(`/bin/file.s.js/raw?file=${file}`).then(response=> response.text())
-  const editor = await dialog.simpleEditDialog(initialValue, async (value) => {
-    let response = await fetch(`/bin/file.s.js/update?file=${file}`, {method: "PUT", body: value})
-    if(response.ok) {
-      lib.notificationFrom(editor, `Success, updated file`, {transient: true})
-      return true
-    } else {
-      lib.notificationFrom(editor, `Error: ${response.status}, ${response.statusText}`, {error:true})
-    }
-  })
-  dropdownCallElem.parentElement.insertAdjacentElement('afterend', editor)
-}
-
-export async function openSimpleAppendDialog(dropdownCallElem) {
-  const lib = await import('/lib/lib.mjs')
-  const dialog = await import('/pagelets/dialog/dialog.mjs')
-  const file   = lib.getParentMatching(dropdownCallElem, '*[data-file]')?.dataset.file
-  if(file === undefined)
-    return void lib.notificationFrom(dropdownCallElem, 'Error, no data-file attribute given', {error: true})
-  // else
-  const editor = await dialog.simpleEditDialog('', async (value) => {
-    let response = await fetch(`/bin/file.s.js/append?file=${file}`, {method: "PUT", body: value})
-    if(response.ok) {
-      lib.notificationFrom(editor, `Success, appended to file`, {transient: true})
-      return true
-    } else {
-      lib.notificationFrom(editor, `Error: ${response.status}, ${response.statusText}`, {error:true})
-    }
-  })
-  dropdownCallElem.parentElement.insertAdjacentElement('afterend', editor)
-}
-
 export async function openAsList(dropdownCallElem) {
   const lib = await import('/lib/lib.mjs')
   const file   = lib.getParentMatching(dropdownCallElem, '*[data-file]')?.dataset.file
