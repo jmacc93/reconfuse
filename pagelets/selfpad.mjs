2ac32eab0b885cbebb7cffd046966e2be5362d05
jmacc93 <joe_miller_93@protonmail.com>
2023-01-09 11:24:32 -0600

    Fixed bug so this actually works

--- pagelets/selfpad.mjs
+++ pagelets/selfpad.mjs
@@ -65,9 +65,11 @@ export async function installFunctionality(callElem) {
 }
   
 export async function updateButton(callElem) {
-  const pagelet = callElem.closest('.selfpad')
+  const lib = await import('/lib/lib.mjs')
+  const pagelet = lib.getParentMatching(callElem, '.selfpad')
   const textarea = pagelet.querySelector('textarea')
   const contentDisplay = pagelet.querySelector('.content-display')
+  contentDisplay.hidden = false
   
   updateLocalStorage(textarea, pagelet)
   renderTextareaContent(textarea, contentDisplay, pagelet)
